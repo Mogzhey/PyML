810c819f460599226ec31c649232ec083fa46dd4
Mogzhey <mogzhey@yahoo.com>
2024-09-10 17:30:48 +0100

    Calculate derivatives of loss and activation functions

--- src/activations.py
+++ src/activations.py
@@ -7,6 +7,9 @@ class Activation:
     def df(self, x: np.ndarray) -> np.ndarray:
         pass
 
+    def __call__(self, x: np.ndarray) -> np.ndarray:
+        return self.f(x)
+
 
 class Linear(Activation):
     def f(self, x: np.ndarray) -> np.ndarray:
@@ -16,28 +19,32 @@ class Linear(Activation):
         return np.ones_like(x)
 
 
-class Sigmoid(Activation):
-    def f(self, x: np.ndarray) -> np.ndarray:
-        return 1 / (1 + np.exp(-x))
-    
-    def df(self, x: np.ndarray) -> np.ndarray:
-        fx = Sigmoid.f(x)
-        return fx * (1 - fx)
-
-
 class ReLU(Activation):
     def f(self, x: np.ndarray) -> np.ndarray:
         return np.maximum(np.zeros_like(x), x)
     
     def df(self, x: np.ndarray) -> np.ndarray:
         return np.where(x <= 0, 0, 1)
+    
+
+class Sigmoid(Activation):
+    def f(self, x: np.ndarray) -> np.ndarray:
+        return 1 / (1 + np.exp(-x))
+    
+    def df(self, x: np.ndarray) -> np.ndarray:
+        fx = self.f(x)
+        return fx * (1 - fx)
 
 
 class Softmax(Activation):
     def f(self, x: np.ndarray) -> np.ndarray:
-        exp = np.exp(x)
+        exp = np.exp(x - np.max(x)) # subtract np.max(x) from arg to make it stable
         return exp / np.sum(exp)
 
     def df(self, x: np.ndarray) -> np.ndarray:
-        s = Softmax.f(x).reshape(-1, 1)
-        return np.diagflat(s) - np.dot(s, s.T)
+        # https://aimatters.wordpress.com/2019/06/17/the-softmax-function-derivative/
+        S = self.f(x)
+        S_vector = S.reshape(-1, 1)
+        S_matrix = np.tile(S_vector, S.shape[0])
+
+        return np.diag(S) - (S_matrix * S_matrix.T)
--- src/losses.py
+++ src/losses.py
@@ -1,17 +1,51 @@
 import numpy as np
 
-def mse(y_true: np.ndarray, y_pred: np.ndarray) -> float:
-    return np.square(y_true - y_pred).mean()
+class Loss:
+    def f(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        pass
 
-def binary_crossentropy(y_true: np.ndarray, y_pred: np.ndarray) -> float:
-    return -1 / y_true.size * np.sum(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+    def df(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        pass
 
-def bce(y_true: np.ndarray, y_pred: np.ndarray) -> float:
-    return binary_crossentropy(y_true, y_pred)
+    def __call__(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        return self.f(y_true, y_pred)
 
-def categorical_crossentropy(y_true: np.ndarray, y_pred: np.ndarray) -> float:
-    # grad = return -y_true/(y_pred)
-    return -np.sum(y_true * np.log(y_pred))
 
-def cce(y_true: np.ndarray, y_pred: np.ndarray) -> float:
-    return categorical_crossentropy(y_true, y_pred)
+class MSE(Loss):
+    def f(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        return np.square(y_true - y_pred).mean()
+
+    def df(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        return -2 * (y_true - y_pred).mean()
+
+
+class BinaryCrossEntropy(Loss):
+    def f(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        return -(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred)).mean()
+
+    def df(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        return -((y_true / y_pred) - ((1 - y_true) / (1 - y_pred))).mean()
+
+
+class BCE(BinaryCrossEntropy):
+    def f(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        return super(y_true, y_pred)
+
+    def df(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        return super.df(y_pred)
+
+
+class CategoricalCrossEntropy(Loss):
+    def f(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        return -np.sum(y_true * np.log(y_pred))
+
+    def df(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        return -np.sum(y_true / y_pred)
+
+
+class CCE(CategoricalCrossEntropy):
+    def f(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        return super(y_true, y_pred)
+
+    def df(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        return super.df(y_pred)
